02f2b40bc79c0d8ec24564bf919eea405cd01ca8
Olivia Guest <guest.olivia@gmail.com>
2024-03-23 21:37:45 +0100

    page

--- openaire.py
+++ openaire.py
@@ -6,14 +6,15 @@ import pandas as pd
 refresh_token = open("secret.txt", "r").read()
 # get your own from: https://develop.openaire.eu/personal-token
 
-def get_openaire_df(keywords, size):
+def get_openaire_df(keywords, size, page):
     # GET https://services.openaire.eu/uoa-user-management/api/users/getAccessToken?refreshToken={your_refresh_token}
     token_response = requests.get('https://services.openaire.eu/uoa-user-management/api/users/getAccessToken?refreshToken=' + refresh_token)
     token_response = json.loads(token_response.content)
     token = 'Bearer {t}'.format(t = token_response['access_token'])
     params = {'keywords': '{t}'.format(t = keywords),
               'format': 'csv',
-              'size': size}
+              'size': size,
+              'page': page}
     # params = {'keywords': 'category, cognition'} # to be clear, I have no idea what this does!
     # it just seems a sensible starting point!
     # to test it try: https://api.openaire.eu/search/researchProducts?keywords=categorisation,cognition
@@ -27,4 +28,4 @@ def get_openaire_df(keywords, size):
 
 
 if __name__ == '__main__':
-    print(get_openaire_df(keywords='categorisation', size=100))
+    print(get_openaire_df(keywords='categorisation', size=100, page=1))
